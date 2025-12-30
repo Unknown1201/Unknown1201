b53123fe4af5cb54b7b5210dc9953c13e0d4ba37
Unknown1201 <devraj.ms812@gmail.com>
2025-12-30 15:18:25 +0530

    Optimize for Vercel Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT:
-  // If deploying to https://Unknown1201.github.io/devraj-portfolio/ use '/devraj-portfolio/'
-  // If deploying to https://Unknown1201.github.io/ (root) use '/'
-  base: '/devraj-portfolio/', 
 })
